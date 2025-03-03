7d45f54d537b9f3d508a9e885b6d60a0fe478fde
tjahjoe <ntell634@gmail.com>
2025-03-03 20:24:41 +0700

    add: P2-View-Membuat View

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function(){
+    return view('hello', ['name' => 'Wahyu']);
+});
