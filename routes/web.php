0bd1ae461343edc2db20070fa50704aab4bb23d8
tjahjoe <ntell634@gmail.com>
2025-03-03 12:56:15 +0700

    fix: reset code in route

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function(){
-    return 'Hello World';
-}); 
-
-Route::get('/world', function(){
-    return 'World';
-});
