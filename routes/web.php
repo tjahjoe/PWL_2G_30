f56445d71fa5bf3a74db25ebaec0aa9879b4df0e
tjahjoe <ntell634@gmail.com>
2025-03-03 15:17:47 +0700

    add: P2-Routing-Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,15 @@ Route::get('/about', function () {
         'Nama' => 'Wahyu Rizky Cahyana'
     ]);
 });
+
+Route::get('/user/{name}', function($name){
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'. $postId . 'Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID ' . $id;
+});
