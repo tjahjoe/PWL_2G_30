d7b9572315f31fffd854c3f987f34ceb2a9ed56e
tjahjoe <ntell634@gmail.com>
2025-03-03 16:38:19 +0700

    fix: P2-Controller-Membuat Controller-Singel Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -21,11 +24,11 @@ Route::get('/world', function(){
     return 'World';
 });
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'about']);
 
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/user/{name?}', function($name='Jhon'){
     return 'Nama saya ' . $name;
