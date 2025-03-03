33f11cab8e9d8a80cbe8dd385657e96dc3dd18cb
tjahjoe <ntell634@gmail.com>
2025-03-03 20:45:16 +0700

    add: P2-View-Menampilkan view dari controller

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,4 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function(){
-    return view('blog.hello', ['name' => 'Wahyu']);
-});
+Route::get('/greeting',[WelcomeController::class, 'greeting']);
