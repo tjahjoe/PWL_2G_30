91134721fa4ded07c91006ffa3973d90f355a701
tjahjoe <ntell634@gmail.com>
2025-03-03 20:08:29 +0700

    add: P2-Controller-Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,3 +38,13 @@ Route::get('/user/{name?}', function($name='Jhon'){
 Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
     return 'Pos ke-'. $postId . 'Komentar ke-' . $commentId;
 });
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
