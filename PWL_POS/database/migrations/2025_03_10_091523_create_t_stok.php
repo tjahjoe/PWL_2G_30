e8fb92b2a19e91a4bb0bc6840b3bab7a308b266d
tjahjoe <ntell634@gmail.com>
2025-03-10 16:38:01 +0700

    add: P3-Pembuatan file migrasi dengan relasi-Migration

---
+++ PWL_POS/database/migrations/2025_03_10_091523_create_t_stok.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+            $table->id('stok_id');
+            $table->unsignedBigInteger('barang_id')->index(); // Fixed type
+            $table->unsignedBigInteger('user_id')->index();   // Fixed type
+            $table->datetime('stok_tanggal');
+            $table->integer('stok_jumlah')->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('m_user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
