b8c66f98defb720949aeeedf8538eb5c1f60b80f
tjahjoe <ntell634@gmail.com>
2025-03-12 16:10:04 +0700

    add: P4-Retreiving or Creating Models-Retrieving Single Models

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,7 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        $amount = UserModel::where('level_id', 2)->count();
-        return view('user', ['data' => $amount]);
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager3',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 1
+            ]
+            );
+        $user->save();
+        return view('user', ['data' => $user]);
     }
 }
