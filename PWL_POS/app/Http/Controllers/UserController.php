4f7f5df61f0da9ce672edc4bfa1787f0b2d49229
tjahjoe <ntell634@gmail.com>
2025-03-19 22:17:10 +0700

    add: P6-Modal Ajax Edit Data (Data User)

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -43,18 +43,29 @@ class UserController extends Controller
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) {
 
-                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                //                 $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+    
+                //                 $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+    
+                //                 $btn .= '<form class="d-inline-block" method="POST" action="' .
+    
+                //                     url('/user/' . $user->user_id) . '">'
+    
+                //                     . csrf_field() . method_field('DELETE') .
+//                     '<button type="submit" class="btn btn-danger btn-sm" onclick="return
+    
+                // confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                $btn = '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
 
-                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                    '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
 
-                $btn .= '<form class="d-inline-block" method="POST" action="' .
+                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
 
-                    url('/user/' . $user->user_id) . '">'
+                    '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
 
-                    . csrf_field() . method_field('DELETE') .
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return
+                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
 
-confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                    '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
 
                 return $btn;
             })
@@ -170,6 +181,14 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
         return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
+    public function edit_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.edit_ajax', ['user' => $user, 'level' => $level]);
+    }
+
     public function update(Request $request, string $id)
     {
         $request->validate([
@@ -187,6 +206,45 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
         ]);
 
         return redirect('/user')->with('success', 'Data user berhasil diubah');
+    }
+
+    public function update_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|max:20|unique:m_user,username,' . $id . ',user_id',
+                'nama' => 'required|max:100',
+                'password' => 'nullable|min:6|max:20'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validaso gagal',
+                    'msgfield' => $validator->errors()
+                ]);
+            }
+            $check = UserModel::find($id);
+            if ($check) {
+                if (!$request->filled('password')) {
+
+                    $request->request->remove('password');
+                }
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
     }
     public function destroy(string $id)
     {
--- PWL_POS/resources/views/user/index.blade.php
+++ PWL_POS/resources/views/user/index.blade.php
@@ -47,7 +47,9 @@
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true" ></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" 
+    role="dialog" data-backdrop="static" data-keyboard="false" 
+    data-width="75%" aria-hidden="true" ></div>
 @endsection
 @push('css')
 @endpush
