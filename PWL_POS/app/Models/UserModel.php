ac9b10294b17b61433ce8f35d0ab931c4d953b7a
tjahjoe <ntell634@gmail.com>
2025-05-06 06:11:01 +0700

    add: P7-Implementasi Authorizaton di Laravel dengan Middleware

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
     
@@ -16,9 +16,20 @@ class UserModel extends Model
 
     protected $fillable = ['level_id', 'username',  'password', 'nama', 'created_at', 'updated_at'];
     protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
     }
 }
--- PWL_POS/resources/views/auth/login.blade.php
+++ PWL_POS/resources/views/auth/login.blade.php
@@ -87,7 +87,7 @@
             $("#form-login").validate({
                 rules: {
                     username: { required: true, minlength: 4, maxlength: 20 },
-                    password: { required: true, minlength: 6, maxlength: 20 }
+                    password: { required: true, minlength: 5, maxlength: 20 }
                 },
                 submitHandler: function (form) { // ketika valid, maka bagian yg akan dijalankan
                     $.ajax({
