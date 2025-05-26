067fe8a400a16293b3229a2b9e7be7e4db748479
tjahjoe <ntell634@gmail.com>
2025-05-26 14:40:46 +0700

    add: P11-Tugas

--- PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,45 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        $validator = Validator::make($request->all(),[
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('posts', 'public');
+            $filename = basename($path);
+        } else {
+            return response()->json(['error' => 'Image file not uploaded'], 400);
+        }
+        
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $filename
+        ]);
+
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false
+        ], 201);
     }
 
     public function show(BarangModel $barang)
--- PWL_POS/app/Models/BarangModel.php
+++ PWL_POS/app/Models/BarangModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,10 +13,17 @@ class BarangModel extends Model
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
     
-    protected $fillable = ['kategori_id', 'barang_kode',  'barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['kategori_id', 'barang_kode',  'barang_nama', 'harga_beli', 'harga_jual', 'image'];
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
